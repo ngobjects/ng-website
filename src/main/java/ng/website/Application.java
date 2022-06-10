256affa756f28a4c5b58a4e1a5134587130d53e5
Hugi Thordarson <hugi@karlmenn.is>
2022-06-10 16:27:18 +0000

    Add another test page

--- src/main/java/ng/website/Application.java
+++ src/main/java/ng/website/Application.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import ng.appserver.NGActionResults;
 import ng.appserver.NGApplication;
+import ng.appserver.NGComponent;
 import ng.appserver.NGRequest;
+import ng.appserver.NGResponse;
 
 public class Application extends NGApplication {
 
@@ -14,8 +16,18 @@ public class Application extends NGApplication {
 	}
 
 	public Application() {
+
+		// This route maps the given request
 		routeTable().map( "/page/", ( request ) -> {
-			return null;
+			final String pageName = request.parsedURI().getString( 1 );
+
+			for( Page page : pages() ) {
+				if( page.id().equals( pageName ) ) {
+					return pageWithName( page.componentClass, request.context() );
+				}
+			}
+
+			return new NGResponse( "Page not found", 404 );
 		} );
 	}
 
@@ -24,13 +36,17 @@ public class Application extends NGApplication {
 		return pageWithName( StartPage.class, request.context() );
 	}
 
-	public record Page( String name, String url ) {};
+	public record Page( String name, String id, Class<? extends NGComponent> componentClass ) {
+
+		public String url() {
+			return "/page/" + id;
+		}
+	}
 
 	public static List<Page> pages() {
 		return Arrays.asList(
-				new Page( "APIs", null ),
-				new Page( "Components", null ),
-				new Page( "WebObjects integration", null ) );
+				new Page( "APIs", "api", null ),
+				new Page( "Documentation", "docs", DocumentationPage.class ),
+				new Page( "WebObjects integration", "wo-integration", null ) );
 	}
-
 }
---
+++ src/main/java/ng/website/DocumentationPage.java
@@ -0,0 +1,11 @@
+package ng.website;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class DocumentationPage extends NGComponent {
+
+	public DocumentationPage( NGContext context ) {
+		super( context );
+	}
+}
