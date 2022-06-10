58ee17a16a9fa2073f90b90034637b574131d1d1
Hugi Thordarson <hugi@karlmenn.is>
2022-06-10 16:29:11 +0000

    Added a Wrapper component

---
+++ src/main/java/ng/website/components/WrapperComponent.java
@@ -0,0 +1,15 @@
+package ng.website.components;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+/**
+ * Wraps the content pages on the site
+ */
+
+public class WrapperComponent extends NGComponent {
+
+	public WrapperComponent( NGContext context ) {
+		super( context );
+	}
+}
