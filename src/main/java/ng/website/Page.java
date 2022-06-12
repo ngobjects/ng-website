cfd7c1c11e8e7d5405a691b7415a93928a61f069
Hugi Thordarson <hugi@karlmenn.is>
2022-06-12 11:58:51 +0000

    Add support for Markdown pages

--- src/main/java/ng/website/Page.java
+++ src/main/java/ng/website/Page.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import ng.appserver.NGComponent;
+import ng.website.Page.Type;
 import ng.website.components.DemoPage;
 import ng.website.components.DocumentationPage;
 import ng.website.components.GettingStartedPage;
@@ -13,7 +14,23 @@ import ng.website.components.WebObjectsIntegrationPage;
  * Represents a content page in our site
  */
 
-public record Page( String name, String id, Class<? extends NGComponent> componentClass ) {
+public record Page( String name, String id, Type type, Class<? extends NGComponent> componentClass, String markdownFilename ) {
+
+	public enum Type {
+		// A plain WebObjects component-type page
+		Component,
+
+		// Renders a resource file in markdown-format
+		Markdown
+	}
+
+	public Page( String name, String id, Class<? extends NGComponent> componentClass ) {
+		this( name, id, Type.Component, componentClass, null );
+	}
+
+	public Page( String name, String id, String markdownFilename ) {
+		this( name, id, Type.Component, null, markdownFilename );
+	}
 
 	/**
 	 * @return The public URL for the oage
@@ -30,7 +47,7 @@ public record Page( String name, String id, Class<? extends NGComponent> compone
 				new Page( "Getting started", "getting-started", GettingStartedPage.class ),
 				new Page( "Documentation", "docs", DocumentationPage.class ),
 				new Page( "Demo", "demo", DemoPage.class ),
-				new Page( "API", "api", null ),
+				new Page( "API", "api", "api.md" ),
 				new Page( "WebObjects integration", "wo-integration", WebObjectsIntegrationPage.class ) );
 	}
 }
