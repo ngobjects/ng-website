0b6dc49561cb33bc887136d786d11f84865ab078
Hugi Thordarson <hugi@karlmenn.is>
2022-06-17 11:17:19 +0000

    Scaffolding for the first version of Dynamic Element documentation

--- src/main/java/ng/website/components/DynamicElementsPage.java
+++ src/main/java/ng/website/components/DynamicElementsPage.java
@@ -1,11 +1,22 @@
 package ng.website.components;
 
+import java.util.List;
+
 import ng.appserver.NGComponent;
 import ng.appserver.NGContext;
+import ng.appserver.elements.docs.NGDynamicElementDescription;
+import ng.appserver.elements.docs.NGDynamicElementDescription.NGBindingDescription;
 
 public class DynamicElementsPage extends NGComponent {
 
+	public NGDynamicElementDescription current;
+	public NGBindingDescription currentBinding;
+
 	public DynamicElementsPage( NGContext context ) {
 		super( context );
+	}
+
+	public List<NGDynamicElementDescription> all() {
+		return NGDynamicElementDescription.all();
 	}
 }
