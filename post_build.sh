774188355b1cc1578a8f3d508dcf9bba77e2e983
Hugi Thordarson <hugi@karlmenn.is>
2022-06-17 00:36:57 +0000

    Point to the correct location on server

--- post_build.sh
+++ post_build.sh
@@ -2,9 +2,9 @@
 
 echo "================== Initiating post_build.sh script =================="
 
-PROJECT_NAME=Rebelliant
+PROJECT_NAME=ng-website
 DESTINATION_SERVER=linode-4.rebbi.is
-DESTINATION_DIRECTORY=/rebbi/com.rebelliant/wo/
+DESTINATION_DIRECTORY=/rebbi/com.fermentedshark/wo/
 OLD_APP_NAME=x$PROJECT_NAME`date "+_%Y_%m_%d_%H_%M_%S.woa"`
 
 date "+%Y-%m-%d %H:%M:%S" | tr -d '\n' > $WORKSPACE/target/$PROJECT_NAME.woa/Contents/Resources/buildDate.txt
